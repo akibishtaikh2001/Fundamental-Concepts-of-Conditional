87fcee99467b9b8d302160ebb24734224ae22b67
akibishtaikh2001 <akibishtiakh2220@gmail.com>
2025-07-29 19:17:03 +0600

    if else condition

---
+++ if.js
@@ -0,0 +1,54 @@
+/**
+ * Tui jodi amake khawash
+ *    taile ami ashbo
+ * 
+ * ------------------
+ * if you buy food for me
+ *      I will come
+ * 
+ * -----------------------
+ * if briyani price less than 300 taka
+ *       I will buy
+ * 
+ * 
+ * -------------STRUCTURE---------------
+ * if(condition){
+ * 
+ * }
+ * 
+ * --------------------------------------
+ * if(condition){
+ * 
+ * }
+ * 
+ */
+
+if(13 <10){
+    // console.log('I have smaller value');
+}
+
+var biriyaniPrice = 250;
+
+
+if(biriyaniPrice < 300){
+    console.log('mama give me some briyani')
+}
+
+
+/**
+ * if(condition){
+ *           execute code if condition adove is true
+ * }
+ * else{
+ *           execute code if condition adove is false
+ * }
+ */
+
+var weight = 20;
+
+if(weight < 20){
+    console.log('I will carry it by myself');
+}
+else{
+    console.log('I will rent a rickshaw');
+}
