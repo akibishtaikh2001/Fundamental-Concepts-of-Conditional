392abb64607d9d3ec8de2003644342ddccc01388
akibishtaikh2001 <akibishtiakh2220@gmail.com>
2025-07-30 23:03:16 +0600

    Use Ternary if-else

---
+++ problem5.js
@@ -0,0 +1,15 @@
+let num1 = 50;
+let num2 = 60;
+
+// if(num1 > num2){
+//     let result = num1 * 2;
+//     console.log(result);
+// }
+// else{
+//     let result = num1 + num2;
+//     console.log(result);
+// }
+
+//---------------TERNARY-NUMBER---------------------
+
+num1> num2 ? console.log(num1 *2) : console.log(num1 + num2);
