ddd00f625548fac8b6498b881fa3c04d65d993cd
akibishtaikh2001 <akibishtiakh2220@gmail.com>
2025-07-30 23:16:02 +0600

    Ticket Calculator

---
+++ problem6.js
@@ -0,0 +1,17 @@
+// Ticket fare Calculator
+
+let age = 7;
+let isStudent = false;
+
+if(isStudent){
+    console.log('You Get a 50% discount');
+}
+else if(age < 10){
+    console.log('free');
+}
+else if( age >= 60){
+    console.log('Gets a 15% Discount');
+}
+else{
+    console.log('Regular ticket fare 800tk');
+}
